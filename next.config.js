4dfa1f604ee5e23f046e8c2394f1a5f60e441b22
deej-0101 <diane.joy.d.n@gmail.com>
2024-06-01 03:47:48 +0800

    try

--- next.config.js
+++ next.config.js
@@ -1,16 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true
-  
-  experimental: {
-
-    appDir: true,
-
+  generateBuildId: async () => {
+    // This could be anything, using the latest git hash
+    return process.env.GIT_HASH
   },
-
-  trailingSlash: true,
-
-};
-
+}
 
 module.exports = nextConfig;
