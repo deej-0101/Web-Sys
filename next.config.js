5ffb3ace2d199c5240731d0add02f2eb1f838a60
deej-0101 <diane.joy.d.n@gmail.com>
2024-06-01 03:38:54 +0800

    modified next.comfig

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  // reactStrictMode: true
+  
+  experimental: {
+
+    appDir: true,
+
+  },
+
+  trailingSlash: true,
+
 };
+
 
 module.exports = nextConfig;
